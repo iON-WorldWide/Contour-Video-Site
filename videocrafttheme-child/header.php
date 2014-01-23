0733954dbd4d610c0c212b96c69526af7311736b
Matt Burdick <matt.s.burdick@gmail.com>
2014-01-22 22:45:11 -0800

    add ga tracking

--- videocrafttheme-child/header.php
+++ videocrafttheme-child/header.php
@@ -1,4 +1,5 @@
     <?php
+
     /**
      * The Header for our theme.
      *
@@ -22,7 +23,7 @@
                 }
                 ?>
                 <?php if (inkthemes_get_option('inkthemes_description') != '') { ?>
-                    <meta name="description" content="<?php echo inkthemes_get_option('inkthemes_description'); ?>" />
+                    
                 <?php
                 } else {
                     
@@ -41,6 +42,9 @@
             <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
             <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" /> 
     <?php wp_head(); ?>
+
+
+
     <!--[if IE 6]>
     <style>
     .feature-post .feature-box {
@@ -70,6 +74,18 @@
             </style>
             <![endif]-->
         </head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-410185-4', 'contour.com');
+  ga('send', 'pageview');
+
+</script>
+
+
         <body>
         <div class="top_strip">
             <div class="container_24">
